18eafc297aea6f84494ad7dbb4cbde7c156e0966
Rafał Michalski <royaltm75@gmail.com>
2016-09-08 19:48:56 +0200

    feature extendable schema types and Enum and Primitive types introduced

--- test/test.schema.js
+++ test/test.schema.js
@@ -139,15 +139,120 @@ test("DB", suite => {
   });
 
   suite.test("should create database with default constraint schema", t => {
-    t.plan(5);
-    var db = new DB({schema: {
+    t.plan(17);
+    var schema = {
       test: {
         name: {type: "string", default: "foo"},
+        enum: {type: "enum", enum: ["foo", "bar"]},
         time: {type: "date", default: () => new Date(2016,6,12,20,42)},
         'other.nested.count': {type: "number", default: 10},
         'other.nested.flag': {type: "boolean", default: true}
       }
-    }});
+    };
+    var db = new DB({schema: schema});
+
+    t.notStrictEqual(db.schema, schema);
+    t.notStrictEqual(db.schema.test, schema.test);
+    t.notStrictEqual(db.schema.test.name, schema.test.name);
+    t.notStrictEqual(db.schema.test.time, schema.test.time);
+    t.notStrictEqual(db.schema.test.enum, schema.test.enum);
+    t.notStrictEqual(db.schema.test['other.nested.count'], schema.test['other.nested.count']);
+    t.notStrictEqual(db.schema.test['other.nested.flag'], schema.test['other.nested.flag']);
+
+    t.deepEqual(db.schema, {
+      test: {
+        name: { type: 'string', default: 'foo' },
+        enum: { type: 'enum', enum: ['foo', 'bar']},
+        time: { type: 'date', default: "() => new Date(2016,6,12,20,42)" },
+        'other.nested.count': { type: 'number', default: 10 },
+        'other.nested.flag': { type: 'boolean', default: true }
+      }
+    });
+
+    t.deepEqual(db.collections.test[Symbol.for('schema')], {
+      "name": {
+        "default": "foo",
+        "name": "name",
+        "prop": "name",
+        "required": false,
+        "type": String,
+      },
+      "enum": {
+        "name": "enum",
+        "prop": "enum",
+        "required": false,
+        "type": {enums: {}}
+      },
+      "other": {
+        "nested": {
+          "count": {
+            "default": 10,
+            "name": "other.nested.count",
+            "prop": "count",
+            "required": false,
+            "type": Number,
+          },
+          "flag": {
+            "default": true,
+            "name": "other.nested.flag",
+            "prop": "flag",
+            "required": false,
+            "type": Boolean,
+          },
+        },
+        "nested.count": {
+          "default": 10,
+          "name": "other.nested.count",
+          "prop": "nested.count",
+          "required": false,
+          "type": Number,
+        },
+        "nested.flag": {
+          "default": true,
+          "name": "other.nested.flag",
+          "prop": "nested.flag",
+          "required": false,
+          "type": Boolean,
+        },
+      },
+      "other.nested": {
+        "count": {
+          "default": 10,
+          "name": "other.nested.count",
+          "prop": "count",
+          "required": false,
+          "type": Number,
+        },
+        "flag": {
+          "default": true,
+          "name": "other.nested.flag",
+          "prop": "flag",
+          "required": false,
+          "type": Boolean,
+        },
+      },
+      "other.nested.count": {
+        "default": 10,
+        "name": "other.nested.count",
+        "prop": "other.nested.count",
+        "required": false,
+        "type": Number,
+      },
+      "other.nested.flag": {
+        "default": true,
+        "name": "other.nested.flag",
+        "prop": "other.nested.flag",
+        "required": false,
+        "type": Boolean,
+      },
+      "time": {
+        "default": "() => new Date(2016,6,12,20,42)",
+        "name": "time",
+        "prop": "time",
+        "required": false,
+        "type": Date,
+      }
+    });
 
     t.type(db, DB);
 
@@ -156,7 +261,7 @@ test("DB", suite => {
       return db.collections.test.createAndSave()
       .then(item => {
         t.type(item, Item)
-        t.deepEqual(item.toJSON(), {_id: item._id, name: 'foo', time: new Date(2016,6,12,20,42), other: {nested: {count: 10, flag: true}}});
+        t.deepEqual(item.toJSON(), {_id: item._id, name: 'foo', enum: null, time: new Date(2016,6,12,20,42), other: {nested: {count: 10, flag: true}}});
         delete item.name;
         delete item.time;
         delete item.other;
@@ -164,7 +269,14 @@ test("DB", suite => {
       })
       .then(item => {
         t.type(item, Item)
-        t.deepEqual(item.toJSON(), {_id: item._id, name: 'foo', time: new Date(2016,6,12,20,42), other: {nested: {count: 10, flag: true}}});
+        t.deepEqual(item.toJSON(), {_id: item._id, name: 'foo', enum: null, time: new Date(2016,6,12,20,42), other: {nested: {count: 10, flag: true}}});
+        item.enum = "bar";
+        return db.save();
+      })
+      .then(item => {
+        t.type(item, Item)
+        t.deepEqual(item.toJSON(), {_id: item._id, name: 'foo', enum: "bar", time: new Date(2016,6,12,20,42), other: {nested: {count: 10, flag: true}}});
+        t.throws(() => { item.enum = "baz"; }, new TypeError('enum: property needs to be one of: "foo","bar"'));
       });
     }).catch(t.throws);
   });
