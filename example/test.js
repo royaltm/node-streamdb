32fcbb2d67eb0ba89359f0d4c43b3516346f2a46
Rafał Michalski <royaltm75@gmail.com>
2016-12-11 03:03:14 +0100

    example: test db

---
+++ example/test.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const DB = require('../lib');
+
+module.exports = function(schemaOnly) {
+  var db = new DB({schema: {
+    test: {
+      name: String,
+      time: Date,
+      'other.nested.count': Number,
+      'other.nested.flag': {type: Boolean, required: true},
+      'other.yet.anything': {type: "*", required: true, default: null}
+    }
+  }});
+
+  if (schemaOnly) return Promise.resolve(db);
+
+  db.stream.pipe(db.stream);
+  return db.writable.then(db => {
+    db.collections.test.create({name: 'foo', time: new Date, other:{nested:{flag:false}}});
+    db.collections.test.create({name: 'bar', time: new Date(0), other:{nested:{flag:true,count:1},yet:{anything:[]}}});
+    return db.save().then(() => db);
+  });
+};
