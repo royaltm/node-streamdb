f746621a6d3245b05c4e066fb058782a2f217402
Rafał Michalski <royaltm75@gmail.com>
2017-09-27 00:37:13 +0200

    extras: fix doc

--- extras/msgpack_lite_codec.js
+++ extras/msgpack_lite_codec.js
@@ -6,7 +6,7 @@ const { createCodec } = require('msgpack-lite');
 const { Ident } = require('../lib/id');
 
 /*
-  This codec for msgpack-lite to save collection items' id as 12 byte buffer.
+  This is codec for msgpack-lite to save collection items' id as 12 byte buffer.
   Assymetrically unpacking Idents as hex strings, more convenient to write to db.stream.
 */
 const codec = module.exports = exports = createCodec({preset: true});
