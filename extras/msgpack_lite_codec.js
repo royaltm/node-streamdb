003ee854c7fbfb9524278130a1286777401eb6c7
Rafał Michalski <royaltm75@gmail.com>
2018-01-12 15:56:20 +0100

    extras: codec and asymmetric coded

--- extras/msgpack_lite_codec.js
+++ extras/msgpack_lite_codec.js
@@ -9,7 +9,11 @@ const { Ident } = require('../lib/id');
   This is codec for msgpack-lite to save collection items' id as 12 byte buffer.
   Assymetrically unpacking Idents as hex strings, more convenient to write to db.stream.
 */
-const codec = module.exports = exports = createCodec({preset: true});
+const asymmetricCodec = exports.asymmetricCodec = createCodec({preset: true});
+const codec = exports.codec = createCodec({preset: true});
+
+asymmetricCodec.addExtPacker(MSGPACK_ID_EXT, Ident, identPacker);
+asymmetricCodec.addExtUnpacker(MSGPACK_ID_EXT, identStringUnpacker);
 
 codec.addExtPacker(MSGPACK_ID_EXT, Ident, identPacker);
 codec.addExtUnpacker(MSGPACK_ID_EXT, identUnpacker);
@@ -19,6 +23,11 @@ function identPacker(ident) {
 }
 
 /* asymmetric unpacker creates strings */
-function identUnpacker(buffer) {
+function identStringUnpacker(buffer) {
   return buffer.toString('hex');
+}
+
+/* symmetric unpacker creates Idents */
+function identUnpacker(buffer) {
+  return new Ident(buffer.toString('hex'));
 }
